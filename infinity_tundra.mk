fa610df36657b0113080129d753823b423ac6023
Shivamingale3 <shivamingale3@gmail.com>
2024-09-14 16:08:41 +0200

    tundra: Updates from T1SJS33M.117-30-3-11 ee9a1f release-keys
    FINGERPRINT: motorola/tundra_g/tundra:13/T1SJS33M.117-30-3-11/ee9a1f:user/release-keys
    
    Signed-off-by: Shivamingale3 <shivamingale3@gmail.com>

--- infinity_tundra.mk
+++ infinity_tundra.mk
@@ -24,10 +24,10 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 30 fusion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tundra_g-user 13 T1SJS33.117-30-3-8 bf62b release-keys" \
+    PRIVATE_BUILD_DESC="tundra_g-user 13 T1SJS33M.117-30-3-11 ee9a1f release-keys" \
     PRODUCT_NAME=tundra_g
 
-BUILD_FINGERPRINT := motorola/tundra_g/tundra:11/T1SJS33.117-30-3-8/bf62b:user/release-keys
+BUILD_FINGERPRINT := motorola/tundra_g/tundra:13/T1SJS33M.117-30-3-11/ee9a1f:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
