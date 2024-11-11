99d9d4b9ce836c9a2ed6ef2724507808da11d5ca
Shivamingale3 <shivamingale3@gmail.com>
2024-11-11 10:48:58 +0530

    tundra: Update build overrides
    Signed-off-by: Shivamingale3 <shivamingale3@gmail.com>

--- infinity_tundra.mk
+++ infinity_tundra.mk
@@ -24,8 +24,8 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := motorola edge 30 fusion
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tundra_g-user 13 T1SJS33M.117-30-3-11 ee9a1f release-keys" \
-    PRODUCT_NAME=tundra_g
+    BuildDesc="tundra_g-user 13 T1SJS33M.117-30-3-11 ee9a1f release-keys" \
+    DeviceProduct=tundra_g
 
 BUILD_FINGERPRINT := motorola/tundra_g/tundra:13/T1SJS33M.117-30-3-11/ee9a1f:user/release-keys
 
